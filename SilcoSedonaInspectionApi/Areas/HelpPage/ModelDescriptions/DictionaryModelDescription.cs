d95fd457de02e85db71906a470cdafb24a220097
Diana Boone <dboone@silcofireprotection.com>
2017-04-05 09:41:31 -0400

    Add project files.

---
+++ SilcoSedonaInspectionApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SilcoSedonaInspectionApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SilcoSedonaInspectionApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SilcoSedonaInspectionApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
